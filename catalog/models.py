4c620d21cb2949bc3cc34198e76ee164e5d25430
Alina <alinatrofimova649@gmail.com>
2023-08-11 14:51:26 +0800

    changing the purchase_price type

--- catalog/models.py
+++ catalog/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     description = models.CharField(max_length=100, verbose_name='описание')
     image = models.ImageField(upload_to='products/', verbose_name='изображеие', null=True, blank=True)
     category = models.CharField(max_length=100, verbose_name='категории')
-    purchase_price = models.CharField(verbose_name='цена за пкупку')
+    purchase_price = models.FloatField(verbose_name='цена за пкупку')
     creation_date = models.TimeField(verbose_name='дата создания', auto_now_add=True)
     last_modified_date = models.TimeField(verbose_name='дата последнего изменения', auto_now=True)
 
