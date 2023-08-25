2d48cdb17a772777384a0a4964523ebe7ec71c2f
Alina <alinatrofimova649@gmail.com>
2023-08-25 19:21:56 +0800

    product model adjustments

--- catalog/models.py
+++ catalog/models.py
@@ -5,7 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
     description = models.CharField(max_length=100, verbose_name='описание')
     image = models.ImageField(upload_to='products/', verbose_name='изображеие', null=True, blank=True)
-    category = models.CharField(max_length=100, verbose_name='категории')
     purchase_price = models.FloatField(verbose_name='цена за пкупку')
     creation_date = models.TimeField(verbose_name='дата создания', auto_now_add=True)
     last_modified_date = models.TimeField(verbose_name='дата последнего изменения', auto_now=True)
